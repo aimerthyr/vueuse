2f0878341676eebbb1b95b80e0862a53e5555004
aimerthyr <aimerthy@foxmail.com>
2025-06-23 22:56:53 +0800

    chore: study provideLocal and injectLocal

--- packages/shared/injectLocal/index.ts
+++ packages/shared/injectLocal/index.ts
@@ -1,25 +1,44 @@
-import { getCurrentInstance, hasInjectionContext, inject } from 'vue'
+import { getCurrentInstance, inject } from 'vue'
 import { localProvidedStateMap } from '../provideLocal/map'
 
+// /**
+//  * On the basis of `inject`, it is allowed to directly call inject to obtain the value after call provide in the same component.
+//  *
+//  * @example
+//  * ```ts
+//  * injectLocal('MyInjectionKey', 1)
+//  * const injectedValue = injectLocal('MyInjectionKey') // injectedValue === 1
+//  * ```
+//  */
+// // @ts-expect-error overloads are not compatible
+// export const injectLocal: typeof inject = (...args) => {
+//   const key = args[0] as string | symbol
+//   const instance = getCurrentInstance()?.proxy
+//   if (instance == null && !hasInjectionContext())
+//     throw new Error('injectLocal must be called in setup')
+
+//   if (instance && localProvidedStateMap.has(instance) && key in localProvidedStateMap.get(instance)!)
+//     return localProvidedStateMap.get(instance)![key]
+
+//   // @ts-expect-error overloads are not compatible
+//   return inject(...args)
+// }
+
 /**
- * On the basis of `inject`, it is allowed to directly call inject to obtain the value after call provide in the same component.
- *
- * @example
- * ```ts
- * injectLocal('MyInjectionKey', 1)
- * const injectedValue = injectLocal('MyInjectionKey') // injectedValue === 1
- * ```
+ * vue 原生的 inject 是无法在当前组件中 provide 后立刻去 inject 使用的
+ * 改造完的函数，会将当前实例存到 weakMap 中，然后在该实例上 挂载 传入的 injectKey 的值
  */
 // @ts-expect-error overloads are not compatible
 export const injectLocal: typeof inject = (...args) => {
-  const key = args[0] as string | symbol
+  const injectKey = args[0]
   const instance = getCurrentInstance()?.proxy
-  if (instance == null && !hasInjectionContext())
-    throw new Error('injectLocal must be called in setup')
-
-  if (instance && localProvidedStateMap.has(instance) && key in localProvidedStateMap.get(instance)!)
-    return localProvidedStateMap.get(instance)![key]
-
+  if (!instance) {
+    throw new Error('injectLocal must use in setup !')
+  }
+  // 如果 weakmap 中存在这个实例的 state 对象，并且提供的 injectKey 也在 state 上面，则直接返回 state 上绑定的值（看一下 injectLocal 就明白了）
+  if (localProvidedStateMap.has(instance) && injectKey in localProvidedStateMap.get(instance)!) {
+    return localProvidedStateMap.get(instance)![injectKey]
+  }
   // @ts-expect-error overloads are not compatible
   return inject(...args)
 }
--- packages/shared/provideLocal/index.ts
+++ packages/shared/provideLocal/index.ts
@@ -1,28 +1,44 @@
-import type { InjectionKey } from 'vue'
 import { getCurrentInstance, provide } from 'vue'
 import { localProvidedStateMap } from './map'
 
 export type ProvideLocalReturn = void
 
-/**
- * On the basis of `provide`, it is allowed to directly call inject to obtain the value after call provide in the same component.
- *
- * @example
- * ```ts
- * provideLocal('MyInjectionKey', 1)
- * const injectedValue = injectLocal('MyInjectionKey') // injectedValue === 1
- * ```
- */
-export function provideLocal<T, K = InjectionKey<T> | string | number>(key: K, value: K extends InjectionKey<infer V> ? V : T): ProvideLocalReturn {
+// /**
+//  * On the basis of `provide`, it is allowed to directly call inject to obtain the value after call provide in the same component.
+//  *
+//  * @example
+//  * ```ts
+//  * provideLocal('MyInjectionKey', 1)
+//  * const injectedValue = injectLocal('MyInjectionKey') // injectedValue === 1
+//  * ```
+//  */
+// export function provideLocal<T, K = InjectionKey<T> | string | number>(key: K, value: K extends InjectionKey<infer V> ? V : T): ProvideLocalReturn {
+//   const instance = getCurrentInstance()?.proxy
+//   if (instance == null)
+//     throw new Error('provideLocal must be called in setup')
+
+//   if (!localProvidedStateMap.has(instance))
+//     localProvidedStateMap.set(instance, Object.create(null))
+
+//   const localProvidedState = localProvidedStateMap.get(instance)!
+//   // @ts-expect-error allow InjectionKey as key
+//   localProvidedState[key] = value
+//   return provide<T, K>(key, value)
+// }
+
+export const provideLocal: typeof provide = (...args) => {
+  const injectKey = args[0] as string | symbol
   const instance = getCurrentInstance()?.proxy
-  if (instance == null)
-    throw new Error('provideLocal must be called in setup')
-
-  if (!localProvidedStateMap.has(instance))
+  if (!instance) {
+    throw new Error('provide must use in setup!')
+  }
+  // 先判断 weakMap 中是否存在该实例
+  if (!localProvidedStateMap.has(instance)) {
+    // 没有的话，就创建一个空对象
     localProvidedStateMap.set(instance, Object.create(null))
-
-  const localProvidedState = localProvidedStateMap.get(instance)!
-  // @ts-expect-error allow InjectionKey as key
-  localProvidedState[key] = value
-  return provide<T, K>(key, value)
+  }
+  const state = localProvidedStateMap.get(instance)!
+  // 将注入的 key 和 value 都挂载到 instance 的 state 对象上面（因为可以有多个 state ）
+  state[injectKey] = args[1]
+  provide(...args)
 }
